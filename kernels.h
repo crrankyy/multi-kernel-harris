d1fc3b00b294436d57ab252c7eb7684363fc5007
crrankyy <82899177+crrankyy@users.noreply.github.com>
2024-05-24 20:38:12 +0530

    Update kernels.h

--- kernels.h
+++ kernels.h
@@ -1,11 +1,11 @@
 #ifndef FUNCTION_KERNELS_H
 #define FUNCTION_KERNELS_H
 
-void sobel_gradient(input_stream<int32> * __restrict in, output_stream<int32> * __restrict out1, output_stream<int32> * __restrict out2);
-void multiply(input_stream<int32> * __restrict ix, input_stream<int32> * __restrict iy, output_stream<int32> * __restrict ixxiyy, output_stream<int32> * __restrict ixy);
-void multiply_and_add(input_stream<int32> * __restrict ixxiyy, output_stream<int32> * __restrict trc, output_stream<int32> * __restrict det1);
-void square(input_stream<int32> * __restrict ixy, output_stream<int32> * __restrict det2);
-void black(input_stream<int32> * __restrict in1, input_stream<int32> * __restrict in2, output_stream<int32> * __restrict out);
-void threshold(input_stream<int32> * __restrict trc, input_stream<int32> * __restrict det, output_stream<int32> * __restrict out);
+void sobel_gradient(input_stream<int32> *in, output_stream<int32> *out1, output_stream<int32> *out2);
+void multiply(input_stream<int32> *ix, input_stream<int32> *iy, output_stream<int32> *ixxiyy, output_stream<int32> *ixy);
+void multiply_and_add(input_stream<int32> *ixxiyy, output_stream<int32> *trc, output_stream<int32> *det1);
+void square(input_stream<int32> *ixy, output_stream<int32> *det2);
+void black(input_stream<int32> *in1, input_stream<int32> *in2, output_stream<int32> *out);
+void threshold(input_stream<int32> *trc, input_stream<int32> *det, output_stream<int32> *out);
 
 #endif
